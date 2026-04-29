814385e91ad2e8a12ec2fb5490a69fddd7283682
indiemellow <indiemellow0@gmail.com>
2026-04-29 15:20:06 +0530

    random poems output on clicking like and disslike

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+
+# this is to register in the namebase of the app so that we can use it in the templates
+app_name = 'main'
+
 urlpatterns = [
     path("", views.index, name="index"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,10 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.db.models import Q
 from .models import poem
-
+import random
 
 
 def index(request):
-    latest_stanza_list = poem.objects.order_by("-pub_date")[:5]
-    context = {"latest_stanza_list": latest_stanza_list}
+    action = request.GET.get('action')
+    # Handle button actions
+    #nothing for now but should log into db later on
+    if action == 'like':
+        pass
+    elif action == 'dislike':
+        pass
+    count = poem.objects.count()
+    if count == 0:
+        random_stanza = None
+    else:
+        random_id = random.randint(1, count)
+        random_stanza = poem.objects.filter(pk__gte=random_id).first()
+    context = {"stanza": random_stanza}
     return render(request, "index.html", context)
