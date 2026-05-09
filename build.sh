c23032fb19ed253fa342b3dbf3036d8599ea3e10
indiemellow <jeromeshaijukc2007@gmail.com>
2026-05-09 16:23:33 +0530

    for first production

--- build.sh
+++ build.sh
@@ -1,10 +1,3 @@
-import dj_database_url
-    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-    ```
-
-### 3. Create a Build Script (`build.sh`)
-Create this in your root folder so Render knows how to "install" your app.
-```bash
 #!/usr/bin/env bash
 set -o errexit
 
